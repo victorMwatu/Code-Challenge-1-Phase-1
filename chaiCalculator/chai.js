23c8905c5f57ac85405e005a32a62dc6fe2eb78e
victorMwatu <victormwatu@gmail.com>
2025-06-06 11:15:12 +0300

    create index.html to test the scripts and completed chai.js challenge

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,27 @@
+//function calculateChaiIngredients that prompts user for input
+//prompts user for number of cups of chai and prints to console the ingredients required
+function calculateChaiIngredients() {
+
+    //prompt user for number of cups they want ingredients for 
+    const numberOfCups = parseInt((prompt("Karibu!, how many cups of chai would you like to make:")), 10);
+    
+    //check if number of cups is a valid number and print ingredients if true
+    if(!!numberOfCups === true) {
+        //calculate ingredients
+        let water = 200 * numberOfCups;
+        let milk = 50 * numberOfCups;
+        let majani = 1 * numberOfCups;
+        let sukari = 2 * numberOfCups;
+
+        //print out to console ingredients
+        console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:\n
+                    Water: ${water} ml\n
+                    Milk: ${milk} ml\n
+                    Tea Leaves (Majani): ${majani} tablespoons\n
+                    Sugar (Sukari): ${sukari} teaspoons`);
+    //if user did not input a number at the prompt at all or in digits
+    } else {
+        console.log("You have not given a number, please make sure your input is a number in digits!");
+    }
+}
+calculateChaiIngredients();
